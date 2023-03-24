407487950d22939abc0bf2bdd734607c6b8c3a8c
lionheart <lionheart@localhost.localdomain>
2023-03-24 20:08:35 +0100

    Implementación clase Usuario

---
+++ FONT/domain/Usuario.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/** Clase Usuario. **/
+public class Usuario {
+	
+	/** Atributos **/
+	
+	private String username;
+
+	/** Constructora **/
+	
+	public Usuario(String username) {
+		this.username = username;
+	}
+	
+	/** Métodos públicos **/
+	
