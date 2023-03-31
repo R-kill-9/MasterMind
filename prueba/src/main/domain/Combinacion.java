99932d3ec3723f50b443ce1b56ac5424214728f9
marcelituu <marcelsanchezroca@gmail.com>
2023-03-31 12:39:26 +0200

    Añado la carpeta prueba

---
+++ prueba/src/main/domain/Combinacion.java
@@ -0,0 +1,44 @@
+package main.domain;
+
+import java.util.ArrayList;
+
+
+
+/** Clase Combinacion. **/
+public class Combinacion {
+	
+	/** Atributos **/
+	private ArrayList<Color> combination;
+
+
+	/** Constructora **/
+	
+	public Combinacion(ArrayList<Color> combination) {
+		/** 1 = CodeMaker, 2 = CodeBreaker**/
+		this.combination = combination; 
+	}
+
+
+	public ArrayList<Color> getCombination() {
+		return combination;
+	}
+
+
+	public void setCombination(ArrayList<Color> combination) {
+		this.combination = combination;
+	}
+	
+	/** Métodos privados **/
+
+	/** Métodos públicos **/
+	/*
+	public setCombinacion(int *comb) {
+        //Creamos esta clase para leer valores por consola
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < tamaño; ++i) {
+            int comb[i] = scanner.nextInt();
+        }
+        return comb;
+	}*/
+
+}
