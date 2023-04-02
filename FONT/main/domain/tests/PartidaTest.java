f1e75c014050798251cc96fd0176f1fc67c0f783
elsa.boix <elsaboix23@gmail.com>
2023-04-02 11:45:55 +0200

    ChangedNivelDifTOAbstract

--- FONT/main/domain/tests/PartidaTest.java
+++ FONT/main/domain/tests/PartidaTest.java
@@ -2,12 +2,26 @@ package main.domain.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import main.domain.Partida;
 
 public class PartidaTest {
-
+	/*Atributos*/
+    private Partida testPartidaAyuda;
+    private Partida testPartidaSinAyuda;
+    
+    /*
+     * Método que se aplica siempre antes de hacer un test
+     * Creamos una partida nueva, de Juan sin ayuda y de rol
+     */
+    @Before
+    public void setUp() {
+        testPartidaAyuda = new Partida(1, "Juan", false, false);
+        testPartidaSinAyuda = new Partida(1, "Juan", false, false);
+    }
+    
 	@Test
 	public void test() {
 		Partida partida = new Partida(0, "Juan", false, false);
