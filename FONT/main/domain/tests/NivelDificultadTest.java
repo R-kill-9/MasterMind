ac97656f093601cef61d13c5d5aeb0777dbb94df
Joel <jmaciasrojas@gmail.com>
2023-04-08 22:40:35 +0200

    NIVEL DIFICULTAD CASI LISTO

--- FONT/main/domain/tests/NivelDificultadTest.java
+++ FONT/main/domain/tests/NivelDificultadTest.java
@@ -5,15 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.domain.NivelDificultadBajo;
+
 public class NivelDificultadTest {
 
 	@Before
 	public void setUp() throws Exception {
-	}
+		nd = new NivelDificultadBajo();
+		}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		  
 	}
 
 }
